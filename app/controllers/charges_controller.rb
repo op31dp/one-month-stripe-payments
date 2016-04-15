bbd7d14c6e4302fb943a905dc41157da896a7aab
op31dp <op31dp@gmail.com>
2016-04-15 15:08:47 -0400

    Passing real parameters from the form to the stripe checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,23 @@
 class ChargesController < ApplicationController
-end
+
+
+def create
+
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :card  => params[:stripeToken]
+	  )
+
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => params[:amount],
+	    :description => 'Growth Hacking Crash Course ',
+	    :currency    => 'usd'
+	  )
+
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to charges_path
+	end
+
+			)

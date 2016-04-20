5fc70daa83232d90226701b4db5dba16cf227b74
op31dp <op31dp@gmail.com>
2016-04-20 15:23:03 -0400

    added email

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,13 @@
+class PurchaseMailer < ActionMailer::Base
+layout 'purchase_mailer'
+
+ default from: "One Month Commerce <op31dp@gmail.com>"
+
+ def purchase_receipt purchase
+ @purchase = purchase
+
+ mail to: purchase.email, subject: "Thanks for your purchase!"
+
+	end
+	
+end
